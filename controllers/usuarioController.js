59ad77be4f214ba610231b4caf96238cad7fe31c
mogmarino <mogmarino@gmail.com>
2022-10-05 12:16:37 -0300

    incluyendo cors

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -6,7 +6,6 @@ const registrar = async (req, res) => {
   // Evitar registros duplicados
   const { email } = req.body;
   const existeUsuario = await Usuario.findOne({ email: email });
-  console.log(existeUsuario);
   if (existeUsuario) {
     const error = new Error("Usuario ya registrado");
     return res.status(400).json({ msg: error.message });
@@ -15,8 +14,10 @@ const registrar = async (req, res) => {
   try {
     const usuario = new Usuario(req.body);
     usuario.token = generarId();
-    const usuarioAlmacenado = await usuario.save();
-    res.json(usuarioAlmacenado);
+    await usuario.save();
+    res.json({
+      msg: "Usuario creado correctamente, revisa tu email para confirmar tu cuenta",
+    });
   } catch (error) {
     console.log(error);
   }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
@@ -10,6 +11,25 @@ app.use(express.json());
 dotenv.config();
 
 conectarDB();
+
+// configurar CORS
+// blacklist , whitelist permitidos al conectarse
+
+const whitelist = ["http://localhost:5173"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      // puede consultar la API
+      callback(null, true);
+    } else {
+      // NO ESTA PERMITIDO EL REQUEST
+      callback(new Error("Error de cors"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 // Routing - use responde a todos los verbos http
 app.use("/api/usuarios", usuarioRoutes);
