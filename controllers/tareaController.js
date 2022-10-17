41ee4fa1106d4443fd2abe0c7894f1642b681890
mogmarino <mogmarino@gmail.com>
2022-10-17 17:44:17 -0300

    Modificando tareas

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -18,6 +18,9 @@ const agregarTarea = async (req, res) => {
 
   try {
     const tareaAlmacenada = await Tarea.create(req.body);
+    // almacenar el id en el proyecto
+    existeProyecto.tareas.push(tareaAlmacenada._id);
+    await existeProyecto.save();
     res.json(tareaAlmacenada);
   } catch (error) {
     console.log(error);
--- models/Proyecto.js
+++ models/Proyecto.js
@@ -26,6 +26,12 @@ const proyectosSchema = mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Usuario",
     },
+    tareas: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tarea",
+      },
+    ],
     colaboradores: [
       {
         type: mongoose.Schema.Types.ObjectId,
