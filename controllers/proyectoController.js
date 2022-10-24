ae0bd2978cb0b8bb5f8e94f753fde75951847fdb
mogmarino <mogmarino@hotmail.com>
2022-10-23 22:45:43 -0300

    ajustando alertas

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -2,9 +2,14 @@ import Proyecto from "../models/Proyecto.js";
 import Usuario from "../models/Usuario.js";
 
 const obtenerProyectos = async (req, res) => {
-  const proyectos = await Proyecto.find()
-    .where("creador")
-    .equals(req.usuario)
+  const proyectos = await Proyecto.find({
+    $or: [
+      { colaboradores: { $in: req.usuario } },
+      { creador: { $in: req.usuario } },
+    ],
+  })
+    // .where("creador")
+    // .equals(req.usuario)
     .select("-tareas");
   res.json(proyectos);
 };
@@ -35,7 +40,12 @@ const obtenerProyecto = async (req, res) => {
     return res.status(404).json({ msg: error.message });
   }
 
-  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+  if (
+    proyecto.creador.toString() !== req.usuario._id.toString() &&
+    !proyecto.colaboradores.some(
+      (colaborador) => colaborador._id.toString() === req.usuario._id.toString()
+    )
+  ) {
     const error = new Error("Acción no válida");
     return res.status(401).json({ msg: error.message });
   }
