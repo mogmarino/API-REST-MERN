7e549f9994a53d924deda1c6de15ca9be6c37c5c
mogmarino <mogmarino@hotmail.com>
2022-10-31 23:17:53 -0300

    avances socket.io

--- index.js
+++ index.js
@@ -39,6 +39,38 @@ app.use("/api/tareas", tareaRoutes);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+const servidor = app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+// socket.io
+import { Server } from "socket.io";
+
+const io = new Server(servidor, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.FRONTEND_URL,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Conectado a socket.io");
+
+  // definir los eventos de socket.io
+  socket.on("abrir proyecto", (proyecto) => {
+    // console.log("Desde el proyecto ", proyecto);
+    socket.join(proyecto);
+
+    // repuesta hacia un cliente especifico
+    // socket
+    //   .to("6349709540268c7577fcc91f")
+    //   .emit("respuesta", { author: "mogmarino" });
+  });
+
+  socket.on("nueva tarea", (tarea) => {
+    console.log(tarea);
+    const proyecto = tarea.proyecto;
+    console.log(proyecto);
+    socket.to(proyecto).emit("tarea agregada", tarea);
+  });
 });
